e9691272cf6afb39ffcaaabfbf9e547315aa1ded
Divjason <sejinpark@tcocs.com>
2023-11-08 18:19:36 +0900

    231108

---
+++ 231108/03jQuery/script.js
@@ -0,0 +1,12 @@
+// $(document).ready(function () {
+//   $("*").css("border", "1px solid #00f");
+// });
+
+$(function () {
+  $("*").css("border", "1px solid #00f");
+  $("#tit").css("background", "#ff0").css("color", "#fff");
+  $(".tit2").css("background", "#f0f").css("border", "2px dashed #fff");
+  $("h3").css("background", "#0ff");
+  $(".tit3, h4").css("color", "#f00");
+  $("h2.tit4").css("background", "gray").css("color", "#fff");
+});
